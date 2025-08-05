68dff11b5526d496d14284a4a1808f6cc5e9be6a
jareddelgado454 <jareddelgadotejada.41@gmail.com>
2025-08-05 08:14:28 -0500

    First commit

---
+++ system-payments-backend/src/test/java/com/lta/systemPayments/SystemPaymentsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lta.systemPayments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemPaymentsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
