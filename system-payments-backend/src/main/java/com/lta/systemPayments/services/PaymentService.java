8e63826c5ab26fac2ff199078ed8e8da598ee8f0
jareddelgado454 <jareddelgadotejada.41@gmail.com>
2025-08-05 10:46:58 -0500

    Payment Service implemented

---
+++ system-payments-backend/src/main/java/com/lta/systemPayments/services/PaymentService.java
@@ -0,0 +1,57 @@
+package com.lta.systemPayments.services;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.lta.systemPayments.entities.Payment;
+import com.lta.systemPayments.entities.Student;
+import com.lta.systemPayments.enums.PaymentStatus;
+import com.lta.systemPayments.enums.TypePayment;
+import com.lta.systemPayments.repositories.PaymentRepository;
+import com.lta.systemPayments.repositories.StudentRepository;
+
+@Service
+@Transactional
+public class PaymentService {
+
+    @Autowired
+    private PaymentRepository paymentRepository;
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    public Payment savePayment(MultipartFile file, double amount, TypePayment type, LocalDate date, String studentCode) throws IOException{
+        Path folderPath = Paths.get(System.getProperty("user.home"),"enset-data", "payments");
+        
+        if(!Files.exists(folderPath)){
+            Files.createDirectories(folderPath);
+        }
+
+        String fileName = UUID.randomUUID().toString();
+
+        Path filePath = Paths.get(System.getProperty("user.home"), "enset-data","payments",fileName+".pdf");
+        
+        Files.copy(file.getInputStream(), filePath);
+
+        Student student = studentRepository.findByCode(studentCode);
+        Payment payment = Payment.builder()
+        .type(type)
+        .status(PaymentStatus.CREATED)
+        .date(date)
+        .student(student)
+        .amount(amount)
+        .file(filePath.toUri().toString())
+        .build();
+
+        return paymentRepository.save(payment);
+    }
+}
